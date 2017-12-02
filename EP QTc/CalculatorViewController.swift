64f58c5bc48710601ff26784a6cf34255d781518
David Mann <mannd@epstudiossoftware.com>
2017-12-02 14:06:15 -0700

    Improve keyboard behavior

--- EP QTc/CalculatorViewController.swift
+++ EP QTc/CalculatorViewController.swift
@@ -8,9 +8,30 @@
 
 import UIKit
 
+// Best way to dismiss keyboard on tap on view.
+// See https://stackoverflow.com/questions/32281651/how-to-dismiss-keyboard-when-touching-anywhere-outside-uitextfield-in-swift
+extension UIViewController
+{
+    func hideKeyboard()
+    {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(
+            target: self,
+            action: #selector(UIViewController.dismissKeyboard))
+        
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard()
+    {
+        view.endEditing(true)
+    }
+}
 
 
-class CalculatorViewController: UIViewController {
+class CalculatorViewController: UIViewController, UITextFieldDelegate {
+    var activeField: UITextField? = nil
+    
+    @IBOutlet var scrollView: UIScrollView!
     @IBOutlet var unitsSegmentedControl: UISegmentedControl!
     
     @IBOutlet var qtTextField: UITextField!
@@ -30,13 +51,45 @@ class CalculatorViewController: UIViewController {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
-        qtTextField.becomeFirstResponder()
+        qtTextField.delegate = self
+        intervalRateTextField.delegate = self
+        ageTextField.delegate = self
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWasShown(_:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillBeHidden(_:)), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        hideKeyboard()
+        view.becomeFirstResponder()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @objc
+    func keyboardWasShown(_ notification: NSNotification) {
+        guard let info = notification.userInfo, let activeField = activeField else {
+            return
+        }
+        if let kbSize =  (info[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue.size {
+            let contentInsets = UIEdgeInsets(top: 0, left: 0, bottom: kbSize.height, right: 0)
+            self.scrollView.contentInset = contentInsets
+            self.scrollView.scrollIndicatorInsets = contentInsets
+            var rect = self.view.frame
+            rect.size.height -= kbSize.height
+            if !rect.contains(activeField.frame.origin) {
+                self.scrollView.scrollRectToVisible(activeField.frame, animated: true)
+            }
+        }
+    }
+    
+    @objc
+    func keyboardWillBeHidden(_ notification: NSNotification) {
+        let contentInsets = UIEdgeInsets()
+        self.scrollView.contentInset = contentInsets
+        self.scrollView.scrollIndicatorInsets = contentInsets
+    }
+    
+
 
     @IBAction func unitsChanged(_ sender: Any) {
         clearFields()
@@ -50,10 +103,11 @@ class CalculatorViewController: UIViewController {
     }
     
     @IBAction func ageChanged(_ sender: Any) {
-        ageLabel.isEnabled = !(ageLabel.text == nil || ageLabel.text!.isEmpty)
+        ageLabel.isEnabled = ageTextField.text != nil && !ageTextField.text!.isEmpty
     }
     
     @IBAction func calculate(_ sender: Any) {
+        validateFields()
     }
     
     @IBAction func clear(_ sender: Any) {
@@ -61,10 +115,17 @@ class CalculatorViewController: UIViewController {
         qtTextField.becomeFirstResponder()
     }
     
+    private func validateFields() {
+        
+    }
+    
     private func clearFields() {
         qtTextField.text = ""
         intervalRateTextField.text = ""
         ageTextField.text = ""
+        ageLabel.isEnabled = false
+        sexSegmentedControl.selectedSegmentIndex = 0  // set sex to "unknown"
+        sexLabel.isEnabled = false
     }
     
     
@@ -77,5 +138,21 @@ class CalculatorViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    // MARK: - Delegates
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        self.view.endEditing(false)
+        return true
+    }
+
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        activeField = textField
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        activeField = nil
+    }
+    
+    
 
 }
