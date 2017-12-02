34688c9bda3666566a13d42739fafbe6e72d8553
David Mann <mannd@epstudiossoftware.com>
2017-12-02 09:25:13 -0700

    Initial Commit

---
+++ EP QTc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EP QTc
+//
+//  Created by David Mann on 12/2/17.
+//  Copyright © 2017 EP Studios. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
