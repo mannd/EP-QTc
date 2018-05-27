d652455083ac81f180a0ddfa6fead288f019c529
David Mann <mannd@epstudiossoftware.com>
2018-05-27 10:39:44 -0600

    Add Help file

---
+++ EP QTc/Controllers/HelpViewController.swift
@@ -0,0 +1,35 @@
+//
+//  HelpViewController.swift
+//  EP QTc
+//
+//  Created by David Mann on 5/27/18.
+//  Copyright © 2018 EP Studios. All rights reserved.
+//
+
+import UIKit
+
+class HelpViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
